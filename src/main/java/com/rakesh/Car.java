62d6b2c3618a2911387d45dae183734981b74fb8
rakeshdama <rakeshd0552@gmail.com>
2021-04-20 23:39:05 +0530

    Constructor Injection

--- src/main/java/com/rakesh/Car.java
+++ src/main/java/com/rakesh/Car.java
@@ -1,9 +1,31 @@
 package com.rakesh;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car implements Vehicle{
+
+    public Car(){
+        System.out.println("Car- constructor");
+    }
+
+    @Autowired
+    Tyre tyre;
+
+    public Tyre getTyre() {
+        return tyre;
+    }
+
+    public void setTyre(Tyre tyre) {
+        this.tyre = tyre;
+    }
+
+    @Override
+    public String toString() {
+        return "Car tyre " + tyre;
+    }
+
     @Override
     public void drive() {
         System.out.println("I'm driving a CAR");
--- src/main/java/com/rakesh/Tyre.java
+++ src/main/java/com/rakesh/Tyre.java
@@ -3,6 +3,9 @@ package com.rakesh;
 public class Tyre {
     private String brand;
 
+    public Tyre(String brand) {
+        this.brand = brand;
+    }
 
     public String getBrand() {
         return brand;
