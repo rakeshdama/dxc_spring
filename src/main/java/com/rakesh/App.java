fc752587014398bf61ac8d877ae176e958d9e72e
rakeshdama <rakeshd0552@gmail.com>
2021-04-21 23:55:21 +0530

    Configuration based on Annotation

--- src/main/java/com/rakesh/App.java
+++ src/main/java/com/rakesh/App.java
@@ -6,9 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Car obj = (Car) context.getBean("car");
+ /*       Car obj = (Car) context.getBean("car");
         obj.drive();
-
+*/
 /*        Tyre tyre = (Tyre) context.getBean("tyre");
         System.out.println(tyre);*/
     }
